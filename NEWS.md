ca61c44083c05fa3ff7ba995120acdb110df45d3
Katharine R. Studholme <krstudholme@smcm.edu>
2026-07-21 14:55:21 -0300

    Notify user on loading if version out of date. Print reminder on loading of functions to access reference materials.

---
+++ NEWS.md
@@ -0,0 +1,10 @@
+# seamoveR (development version)
+
+* Notify user on loading if version out of date
+* Print reminder on loading of functions to access reference materials
+
+# seamoveR 0.1.0
+
+* Initial functions for Pathtrack, TechnoSmart, and Ecotone GPS archival and basestation data.
+* Initial development.
+ 
--- R/zzz.R
+++ R/zzz.R
@@ -29,11 +29,36 @@ check_seamoveR_version <- function() {
 
     if (length(remote_version) == 1 &&
         package_version(remote_version) > package_version(installed_version)) {
-      packageStartupMessage(
+
+      msg <- paste0(
         "\nA newer version of seamoveR is available (",
         remote_version, " > ", installed_version, ").\n",
         "Update with: remotes::install_github('krstudholme/seamoveR')\n"
       )
+
+      news_bullets <- tryCatch({
+        news_lines <- readLines(
+          "https://raw.githubusercontent.com/krstudholme/seamoveR/main/NEWS.md",
+          warn = FALSE
+        )
+        start <- grep(paste0("^# seamoveR ", remote_version, "\\b"), news_lines)
+        if (length(start) > 0) {
+          rest <- news_lines[(start[1] + 1):length(news_lines)]
+          end <- which(grepl("^# seamoveR", rest))[1]
+          bullets <- if (!is.na(end)) rest[seq_len(end - 1)] else rest
+          bullets <- bullets[nzchar(trimws(bullets))]
+          if (length(bullets) > 0) bullets else NULL
+        } else {
+          NULL
+        }
+      }, error = function(e) NULL)
+
+      if (!is.null(news_bullets)) {
+        msg <- paste0(msg, "\nWhat's new:\n", paste(news_bullets, collapse = "\n"), "\n")
+      }
+
+      packageStartupMessage(msg)
+
     }
   }, condition = function(e) invisible(NULL))
 }
